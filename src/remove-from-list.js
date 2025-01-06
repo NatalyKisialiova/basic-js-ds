9aa5bde57a23f381c638c57c4d14d5b64b70581d
ŅatalyKisialiova <nataly.kisialiova@gmail.com>
2025-01-06 21:34:13 +0200

    feat: add resolving remove-from-list.js task, add resolving metod root() in the binary-search-tree.js task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError, ListNode } = require('../extensions/index.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,9 +22,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function removeKFromList(l, k ) {
+  while(l != null && l.value === k) {
+    l = l.next
+  }
+  
+  let current = l;
+  while (current !== null && current.next !== null) { 
+    let nextNode = current.next;
+    if (nextNode.value === k) { 
+      current.next = nextNode.next; 
+    } else { 
+      current = nextNode; 
+    } 
+  } return l;
 }
 
 module.exports = {
